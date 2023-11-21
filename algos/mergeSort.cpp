256daed8cd68c0d452b2ed769648cd864b8e58bf
amanhsn <zilleh67@gmail.com>
2023-11-21 11:09:57 +0500

    Sorting Algorithims.

--- algos/mergeSort.cpp
+++ algos/mergeSort.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int arr[10] = {1, 6, 4, 3, 0, 7, 8, 4, 3, 9};
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+
+    int i = 0;
+    int j = 0; 
+    int k = left; 
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+       while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        
+        int mid = left + (right - left) / 2;
+
+       
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int arr[] = {1, 6, 4, 3, 0, 7, 8, 4, 3, 9};
     int length = sizeof(arr) / sizeof(arr[0]);
 
     cout << "Original array: ";
@@ -9,7 +61,7 @@ int main(){
         cout << arr[i] << " ";
     }
 
-    insertionSort(arr, length);
+    mergeSort(arr, 0, length - 1);
 
     cout << "\nSorted array: ";
     for (int i = 0; i < length; i++) {
